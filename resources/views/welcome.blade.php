1dea14065a54b58d39d1723546448b96c6a8d6f4
Jonathan <jonathanrautenbach@gmail.com>
2017-01-24 11:49:41 +0200

    starting blog homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,27 +65,6 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="/about">About</a>
-                    <a href="/">Home</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="/contact">Contact</a>
-                </div>
-            </div>
-        </div>
+       
     </body>
 </html>
